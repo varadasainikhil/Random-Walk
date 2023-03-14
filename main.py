a763a51373a14dfcc337d27ab1437f1db18dab08
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-03-14 17:06:23 +0530

    Random Walk Removed screensize Completely randomised the colors with a function

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import random
 import turtle as t
 from random import choice
+
 t.colormode(255)
+
 # Setting the number of iterations
 iterations = 100
 
 turtle = t.Turtle()
 screen = t.Screen()
-screen.setup(400, 400)
 turtle.shape("arrow")
 turtle.speed("fastest")
 direction = [0, 90, 180, 270, 360]
 turtle.width(5)
 
 
+def random_color():
+    r = random.randint(1, 255)
+    g = random.randint(1, 255)
+    b = random.randint(1, 255)
+    color = (r, g, b)
+    return color
+
+
 for i in range(iterations):
     turtle.setheading(choice(direction))
     turtle.forward(30)
-    turtle.pencolor(random.randint(1,255), random.randint(1,255), random.randint(1,255))
+    turtle.pencolor(random_color())
 
 screen.exitonclick()
-

613899d7c28fcc83c7bdf819178210cdc91e9216
Sai Nikhil Varada <varadasainikhil@gmail.com>
2023-03-14 16:30:10 +0530

    Complete Random walk

---
+++ main.py
@@ -0,0 +1,21 @@
+from turtle import Turtle, Screen
+from random import choice
+
+# Setting the number of iterations
+iterations = 100
+
+turtle = Turtle()
+screen = Screen()
+screen.setup(400,400)
+turtle.shape("arrow")
+color = ["red", "blue", "yellow", "black", "orange", "purple", "brown", "pink"]
+direction = [0, 90, 180, 270, 360]
+turtle.width(5)
+
+for i in range(iterations):
+    turtle.setheading(choice(direction))
+    turtle.forward(30)
+    turtle.color(choice(color))
+
+screen.exitonclick()
+
